2dfc25ce8ba898f76b2815070557561526df27bb
Adarsh Kannada <adarshkannada@outlook.com>
2022-09-17 23:33:56 +0530

    add file copy utils

---
+++ src/util/copy_common_files.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+
+def copy_common_files(path_to_files: str, path_to_source_folder: str, path_to_destination_folder: str):
+    path_to_files = path_to_files
+    list_of_files_in_source = os.listdir(path_to_files)
+    print(list_of_files_in_source, sep='\n')
+
+    path_to_source_folder = path_to_source_folder
+    list_of_all_files = os.listdir(path_to_source_folder)
+    print(list_of_all_files, sep='\n')
+    path_to_destination_folder = path_to_destination_folder
+
+    # getting common photos between two location
+    c = set(list_of_files_in_source) & set(list_of_all_files)
+    s = list(c)
+    print('the number of common photos: ', len(c))
+
+    # create destination folder if it is not present
+    is_present = os.path.exists(path_to_destination_folder)
+    if not is_present:
+        os.makedirs(path_to_destination_folder)
+
+    # copying the photos from source to destination
+    for element in range(0, len(s)):
+        shutil.copy(path_to_source_folder + str(s[element]), path_to_destination_folder + str(s[element]))
+    print("Files are copied successfully")
+
+
+copy_common_files(path_to_files="C://Users//Admin//Downloads//Adarsh//selected-adarsh",
+                  path_to_source_folder="D://MyCamera//2021//Aug//maduve//Adarsha and Swathi Wedding//Photos//",
+                  path_to_destination_folder="D://MyCamera//2021//Aug//maduve//Adarsha and Swathi Wedding//Photos//selected//")
