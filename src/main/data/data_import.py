91a3d6c512c6ce6a67f13bea0af726701407bf7a
adarshkannada <adarshkannada@outlook.com>
2023-07-28 00:05:10 +0530

    code clean up

--- src/main/data/data_import.py
+++ src/main/data/data_import.py
@@ -23,23 +23,33 @@ class ImportData:
         logger.info("data obtained from the source")
         return df1
 
+    def full_load(self, start_year: int, end_year: int):
+        for year in range(start_year, end_year):
+            for month in range(1, 13):
+                try:
+                    Sql().create_table(name=Utils().get_month_year(month, year))
+                    df = ImportData().get_data(rows=32, worksheet=Utils().get_month_year(month, year), header_col_num=1)
+                    Sql().load_data_to_table(table_name=Utils().get_month_year(month, year), data=df)
+                    logger.info(f"data loaded for the month-year {(Utils().get_month_year(month, year))}")
+                except ValueError:
+                    continue
+                if month > 12:
+                    break
+            if year > 2023:
+                break
+
+    def incr_load(self):
+        try:
+            Sql().create_table(name=Utils().get_current_month_year())
+            df = ImportData().get_data(rows=32, worksheet=Utils().get_current_month_year(), header_col_num=1)
+            Sql().load_data_to_table(table_name=Utils().get_current_month_year(), data=df)
+            logger.info(f"data loaded for the month-year {(Utils().get_current_month_year())}")
+        except Error:
+            pass
+
 
 # download_file_by_name(file_name='Expensify')
 # file_rename()
-# data = ImportData().get_data(rows=32, worksheet=Utils().get_current_month_year(), header_col_num=1)
-# print(data)
-# Sql().load_data_to_table(data=data)
 
-for year in range(2019, 2024):
-    for month in range(1, 13):
-        try:
-            # df = ImportData().get_data(rows=32, worksheet=Utils().get_month_year(month, year), header_col_num=1)
-            # Sql().load_data_to_table(data=df)
-            logger.info("data loaded for the month-year", (Utils().get_month_year(month, year)))
-        except ValueError:
-            continue
-        if month > 12:
-            break
-    if year > 2023:
-        break
+
 
--- src/main/data/sqlite_conn.py
+++ src/main/data/sqlite_conn.py
@@ -10,30 +10,27 @@ class Sql:
         try:
             conn = sqlite3.connect('finance.db')
             return conn
-        except Error:
-            print(Error)
+        except sqlite3.Error as error:
+            logger.info("Error while creating a sqlite table", error)
 
     def create_table(self, name: str):
         con = Sql().sql_connection()
         cur = con.cursor()
         # below is incomplete, needs work
-        cur.execute(f"CREATE TABLE IF NOT EXISTS {name}(Date str PRIMARY KEY, Home float,"
+        cur.execute(f"CREATE TABLE IF NOT EXISTS '{name}' (Date str PRIMARY KEY, Home float,"
                     f"Bills float, Shopping float, Food float, Travel float, Clothes float, Fuel float,"
                     f"Gifts float, General float, Investment float, Medicine float, Total float)")
 
-    def load_data_to_table(self, data: any):
+    def load_data_to_table(self, table_name: str, data: any):
         con = Sql().sql_connection()
         logger.info("source dataframe obtained")
         try:
-            data.to_sql(name='monthlyExpense', con=con, if_exists='append', index=False)
+            data.to_sql(name=table_name, con=con, if_exists='append', index=False)
             logger.info("data loaded into the table")
         except sqlite3.IntegrityError:
-            data.to_sql(name='monthlyExpense', con=con, if_exists='replace', index=False)
-            Sql().create_table()
+            Sql().create_table(name=table_name)
+            data.to_sql(name=table_name, con=con, if_exists='replace', index=False)
         logger.info("dropped, created new table. Data loaded again.")
-
-    def full_load(self):
-        con = con = Sql().sql_connection()
 
 
 # Sql().create_table()
